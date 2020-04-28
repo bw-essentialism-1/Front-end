214f94518fdc51cf0490c23ce24c851e2cd02ecd
unknown <jethanhargrave@gmail.com>
2020-04-28 00:42:43 -0400

    login/registration rendering, token is returning undefined - going to try and refactor other code with no redux

--- client/src/components/ValueList.js
+++ client/src/components/ValueList.js
@@ -1,27 +1,25 @@
-import React from "react";
-import { connect } from "react-redux";
-import { getValues } from "../actions";
+// import React from "react";
+// import { connect } from "react-redux";
+// import { getValues } from "../actions";
 
-const ValueList = props => {
+// const ValueList = props => {
 
-    [state, setState] = useState([])
+//     return(
+//         <div>
+//             {props.values.map(item => (
+//                 <div key={item.id}>
+//                     <h2>{item.value}</h2>
+//                 </div>
+//             ))}
+//         </div>
+//     )
+// }
 
-    return(
-        <div>
-            {props.values.map(item => (
-                <div key={item.id}>
-                    <h2>{item.value}</h2>
-                </div>
-            ))}
-        </div>
-    )
-}
+// const mapStateToProps = state => {
+//     return{
+//         values: state.values,
+//         error: state.error
+//     }
+// }
 
-const mapStateToProps = state => {
-    return{
-        values: state.values,
-        error: state.error
-    }
-}
-
-export default connect(mapStateToProps, { getValues })(ValueList);
+// export default connect(mapStateToProps, { getValues })(ValueList);
