ef503dee0540baa34d3837342022741b020f1d40
isaac stott <isaac.cloyd@gmail.com>
2020-04-30 12:42:18 -0600

    added styling, met mvp,

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -15,104 +15,53 @@
 //password
 //submit button
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+// import axios from 'axios';
 import  axiosWithAuth from '../utils/axiosWithAuth';
-import * as yup from 'yup';
-
-const initialFormErrors = {
-  username: '',
-  password: '',
-}
-
-
-const formSchema = yup.object({
-  username: yup
-    .string()
-    .min(3, 'userame must be atleast 3 characters')
-    .required('userName is required'),
-  password: yup
-    .string()
-    .min(6, 'Password Must be at least 6 characters long')
-    .required('Password is Required')
-})
 
 const RegistrationForm = props => {
+  const {
+    setActive,
+    credentials,
+    handleChanges,
+    formDisabled,
+    formErrors,
+  } = props
 
-console.log(props)
-
-    const [credentials, setCredentials] = useState({
-        username: '',
-        password: '',
-
-    });
-
-    const [formErrors, setFormErrors] = useState(initialFormErrors)
-    const [formDisabled, setFormDisabled] = useState(true)
-
-    const handleChanges = event => {
-
-      const name = event.target.name
-      const value = event.target.value
-
-      yup
-      .reach(formSchema, name)
-      .validate(value)
-      .then(valid => {
-        setFormErrors({
-          ...formErrors,
-          [name]: '',
+  const submitForm = event => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post('https://bw-essentialism-1.herokuapp.com/api/auth/register', credentials)
+      .then(res => {
+        console.log(res)
+        setActive(true);
         })
-      })
-      .catch(err => {
-        setFormErrors({
-          ...formErrors,
-          [name]: err.errors[0]
-        })
-      });
-
-      setCredentials({
-        ...credentials,
-        [name]: value,
-     })
-  }
-
-    const submitForm = event => {
-      event.preventDefault();
-      axiosWithAuth()
-        .post('https://bw-essentialism-1.herokuapp.com/api/auth/register', credentials)
-        .then(res => {
-          console.log(res)
-          props.setActive(true);
-          })
-        .catch(err => console.log(err));
-    };
-
-    useEffect(() => {
-      formSchema.isValid(credentials)
-        .then(valid => {
-          setFormDisabled(!valid)
-        })
-    }, [credentials])
+      .catch(err => console.log(err));
+  };
 
   return (
-<>
-<div className='errors'>
-    <p>{formErrors.username}</p>
-    <p>{formErrors.password}</p>
-</div>
-    
     <div>
-      <form onSubmit={submitForm}>
-        <div>Username:</div>
-        <input name="username" type="text" value={credentials.username} onChange={handleChanges} />
-        <div>Password: </div>
-        <input name="password" type="password" value={credentials.password} onChange={handleChanges} />
-        <br/>
-        <button disabled={formDisabled}>Register</button>
-      </form>
+      <h5>Register Here</h5>
+
+      <div className='errors'>
+        <p>{formErrors.username}</p>
+        <p>{formErrors.password}</p>
+        {/* <p>{formErrors.confirmPassword}</p> */}
+      </div>
+
+      <div className="registration">
+        <form onSubmit={submitForm}>
+          <div>Username:</div>
+          <input name="username" type="text"  placeholder=' username' value={credentials.username} onChange={handleChanges} />
+          <div>Password: </div>
+          <input name="password" type="password" placeholder=' password' value={credentials.password} onChange={handleChanges} />
+          {/* <div>Confirm Password: </div>
+          <input name="confirmPassword" type="password" placeholder=' confirm password' value={credentials.confirmPassword} onChange={handleChanges} />
+          */}
+          <button disabled={formDisabled}>Register</button>
+        </form>
+      </div>
     </div>
-</>
   );
 }
 
