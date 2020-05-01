d58bee5a46e80797087d3433c07e3f77e305bb2f
unknown <jethanhargrave@gmail.com>
2020-04-30 22:14:36 -0400

    repushing commented out code to trace bug

--- src/components/ValueForm.js
+++ src/components/ValueForm.js
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
-import axiosWithAuth from "../utils/axiosWithAuth";
+// import React, { useState } from 'react';
+// import axiosWithAuth from "../utils/axiosWithAuth";
 
-const ValueForm = ({ fetchEssentials }) => {
-    const newEssential = {name: '', id: Date.now()}
+// const ValueForm = ({ fetchEssentials }) => {
+//     const newEssential = {name: '', id: Date.now()}
 
-    const [essential, setEssential] = useState(newEssential);
+//     const [essential, setEssential] = useState(newEssential);
 
-    const handleChanges = e => {
-        setEssential({[e.target.name]: e.target.value})
-    }
+//     const handleChanges = e => {
+//         setEssential({[e.target.name]: e.target.value})
+//     }
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        axiosWithAuth().post("https://bw-essentialism-1.herokuapp.com/api/essentials/", essential)
-            .then(res => {
-            fetchEssentials();
-            setEssential(newEssential)
-            console.log(res)
-        })
-            .catch(err => {
-            console.log(err)
-        })
-    }
+//     const handleSubmit = e => {
+//         e.preventDefault();
+//         axiosWithAuth().post("https://bw-essentialism-1.herokuapp.com/api/essentials/", essential)
+//             .then(res => {
+//             fetchEssentials();
+//             setEssential(newEssential)
+//             console.log(res)
+//         })
+//             .catch(err => {
+//             console.log(err)
+//         })
+//     }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text"
-                        name="name"
-                        placeholder="Add values..."
-                        value={essential.name}
-                        onChange={handleChanges}
-                        />
+//     return (
+//         <div>
+//             <form onSubmit={handleSubmit}>
+//                 <input type="text"
+//                         name="name"
+//                         placeholder="Add values..."
+//                         value={essential.name}
+//                         onChange={handleChanges}
+//                         />
 
-                <button type='submit'>+</button>
-            </form>
-        </div>
-    )
+//                 <button type='submit'>+</button>
+//             </form>
+//         </div>
+//     )
 
-}
+// }
 
-export default ValueForm
+// export default ValueForm
--- src/components/ValueList.js
+++ src/components/ValueList.js
@@ -1,72 +1,72 @@
-import React from "react";
-import axiosWithAuth from "../utils/axiosWithAuth"
-import { useState, useEffect } from "react";
-import ValueForm from "./ValueForm"
+// import React from "react";
+// import axiosWithAuth from "../utils/axiosWithAuth"
+// import { useState, useEffect } from "react";
+// import ValueForm from "./ValueForm"
 
 
-const ValueList = () => {
+// const ValueList = () => {
 
-    const [essentialsList, setEssentialsList] = useState([]);
+//     const [essentialsList, setEssentialsList] = useState([]);
 
-    const fetchEssentials = () => {
-        axiosWithAuth()
-            .get("https://bw-essentialism-1.herokuapp.com/api/essentials/")
-            .then(res => {
-                console.log(res.data)
-                setEssentialsList(res.data);
-            })
-            .catch(err => {
-                console.log(err.response)
-            })
-    }
+//     const fetchEssentials = () => {
+//         axiosWithAuth()
+//             .get("https://bw-essentialism-1.herokuapp.com/api/essentials/")
+//             .then(res => {
+//                 console.log(res.data)
+//                 setEssentialsList(res.data);
+//             })
+//             .catch(err => {
+//                 console.log(err.response)
+//             })
+//     }
 
-    const deleteEss = deletedEssential => {
-        const newEssentials = [...essentialsList];
-        const deletedEssentials = newEssentials.filter(item => item.id !== deletedEssential)
-        setEssentialsList(deletedEssentials);
-        console.log(deletedEssentials)
-      }
+//     const deleteEss = deletedEssential => {
+//         const newEssentials = [...essentialsList];
+//         const deletedEssentials = newEssentials.filter(item => item.id !== deletedEssential)
+//         setEssentialsList(deletedEssentials);
+//         console.log(deletedEssentials)
+//       }
 
-    const deleteEssentials = essential => {
-        axiosWithAuth()
-            .delete(`https://bw-essentialism-1.herokuapp.com/api/essentials/${essential.id}`)
-            .then(res => {
-                console.log(res.data)
-            })
-            .catch(err => {
-                console.log(err.response)
-            })
-    }
-
-
-    useEffect(() => {
-        fetchEssentials();
-    }, []);
+//     const deleteEssentials = essential => {
+//         axiosWithAuth()
+//             .delete(`https://bw-essentialism-1.herokuapp.com/api/essentials/${essential.id}`)
+//             .then(res => {
+//                 console.log(res.data)
+//             })
+//             .catch(err => {
+//                 console.log(err.response)
+//             })
+//     }
 
 
-    console.log(essentialsList)
+//     useEffect(() => {
+//         fetchEssentials();
+//     }, []);
 
 
-    return(
-        <div className="vals">
-            <h1>Essentials</h1>
-            <form>
-            {
-             essentialsList.map(item => (
-                <div className="vList">
-                    <label className="vLabel">{item.name}</label>
-                    <button className="vButton" onClick={(e) => {
-                    e.stopPropagation();
-                    deleteEssentials(item)
-                    deleteEss(item.id)
-                  }}>x</button>
-              </div>
-            ))
-            }
-            <ValueForm fetchEssentials={fetchEssentials}/>
-            </form>
-        </div>
-    )
-}
+//     console.log(essentialsList)
 
-export default ValueList
+
+//     return(
+//         <div className="vals">
+//             <h1>Essentials</h1>
+//             <form>
+//             {
+//              essentialsList.map(item => (
+//                 <div className="vList">
+//                     <label className="vLabel">{item.name}</label>
+//                     <button className="vButton" onClick={(e) => {
+//                     e.stopPropagation();
+//                     deleteEssentials(item)
+//                     deleteEss(item.id)
+//                   }}>x</button>
+//               </div>
+//             ))
+//             }
+//             <ValueForm fetchEssentials={fetchEssentials}/>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default ValueList
